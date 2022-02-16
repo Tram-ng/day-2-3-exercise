809097bfc17295baf1d3b050158962f2d5c00f38
Tracie Nguyen <91838259+Tram-ng@users.noreply.github.com>
2022-02-16 22:10:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+y = 90 - int(age)
+m = 12 * y
+w = 52 * y 
+d = 365 * y
+print(f"Congrats! You have {y} years, {m} months, {w} weeks, {d} days left." )
+
+
+
+
+
+
+
+
